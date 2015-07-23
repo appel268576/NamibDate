bd0c9029d85181ee67e9074db3554f4233ce9914
Arno Smit <appel268576@gmail.com>
2015-07-23 08:58:48 +0200

    implement protocol functions

--- NamibDate/CardsViewController.swift
+++ NamibDate/CardsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CardsViewController: UIViewController {
+class CardsViewController: UIViewController, SwipeViewDelegate {
 
     let frontCardTopMargin: CGFloat = 0
     let backCardTopMargin: CGFloat = 10
@@ -25,9 +25,11 @@ class CardsViewController: UIViewController {
         
         // Do any additional setup after loading the view.
         backCard = SwipeView(frame: createCardFrame(backCardTopMargin))
+        backCard!.delegate = self
         cardStackView.addSubview(backCard!)
         
         frontCard = SwipeView(frame: createCardFrame(frontCardTopMargin))
+        frontCard!.delegate = self
         cardStackView.addSubview(frontCard!)
         
     }
@@ -40,6 +42,21 @@ class CardsViewController: UIViewController {
     // Helper Func
     private func createCardFrame(topMargin: CGFloat) -> CGRect {
         return CGRect(x: 0, y: topMargin, width: cardStackView.frame.width, height: cardStackView.frame.height)
+    }
+    
+    // Mark: SwipeViewDelegate
+    func swipedLeft() {
+        println("Left")
+        if let frontCard = frontCard {
+            frontCard.removeFromSuperview()
+        }
+    }
+    
+    func swipedRight() {
+        println("Right")
+        if let frontCard = frontCard {
+            frontCard.removeFromSuperview()
+        }
     }
 
 }
