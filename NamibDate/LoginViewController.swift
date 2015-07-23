0f20d1e2605d6bea7159fb3a4757d960213b4e55
Arno Smit <appel268576@gmail.com>
2015-07-23 15:59:24 +0200

    Create LoginViewController and hook up login button

---
+++ NamibDate/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  NamibDate
+//
+//  Created by Arno Smit on 23/07/15.
+//  Copyright (c) 2015 Namib Lost in Sweden. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func pressedFBLogin(sender: UIButton) {
+        
+    }
+    
+    
+}
