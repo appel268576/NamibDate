c3c37583d189041263c7a8edecc2d45ad05b40d5
Arno Smit <appel268576@gmail.com>
2015-07-23 15:42:40 +0200

    Parse installation

---
+++ NamibDate/Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Bridging-Header.h
+//  NamibDate
+//
+//  Created by Arno Smit on 23/07/15.
+//  Copyright (c) 2015 Namib Lost in Sweden. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+#import <ParseFacebookUtils/PFFacebookUtils.h>
+#import "FacebookSDK/FacebookSDK.h"
